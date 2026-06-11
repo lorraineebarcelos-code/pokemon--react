d8428cc9bdb64fce04df3d6e08d8798676191ee4
Lorraine Barcelos <lorraineebarcelos@gmail.com>
2026-06-10 21:22:46 -0300

    Adiciona imagem de preview

---
+++ POKEMON/src/Data/pages/home.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "../../App.css";
+import Pokemoncard from "../../Components/Pokemoncard/Pokemoncard";
+import pokemons from "../Pokemons";
+
+function Home() {
+
+    const [listaPokemons, setListaPokemons] = useState([
+      { nome: "charmander", estagio: 1, imagem: pokemons['charmander'].imagem } ,
+      { nome: "squirtle", estagio: 1, imagem: pokemons['squirtle'].imagem } ,
+      { nome: "bulbasauro", estagio: 1, imagem: pokemons['bulbasauro'].imagem }
+    ])
+
+  function EvoluirPokemon(nomePokemon) {
+
+    console.log(nomePokemon)
+
+    const novalista = listaPokemons.map((Pokemon) => {
+
+      if (Pokemon.nome !== nomePokemon) {
+        return Pokemon
+      }
+
+      const proximaEvolucao = pokemons[Pokemon.nome].Evolucao
+
+      if (!proximaEvolucao) {
+        return Pokemon
+      }
+
+      return {
+        nome: proximaEvolucao,
+        estagio: Pokemon.estagio + 1,
+        imagem: pokemons[proximaEvolucao].imagem
+      }
+    })
+
+    setListaPokemons(novalista)
+  }
+
+  return (
+    <>
+      <h1>Ola Mundo :D</h1>
+
+      <section id="center">
+        {listaPokemons.map((Pokemon) => (
+          <Pokemoncard
+            key={Pokemon.nome}
+            nome={Pokemon.nome}
+            estagio={Pokemon.estagio}
+            imagem={pokemons[Pokemon.nome].imagem}
+            evoluirPokemon={EvoluirPokemon}
+          />
+        ))}
+      </section>
+    </>
+  )
+}
+
+export default Home;
---
+++ POKEMON/src/Data/pages/pokemon.jsx
@@ -0,0 +1,9 @@
+function Pokemon() {
+    return (
+        <>
+            <h1>Pokemon</h1>
+        </>
+    )
+}
+
+export default Pokemon;
